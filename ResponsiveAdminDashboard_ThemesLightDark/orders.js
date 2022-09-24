f664fa579121dec64e11381cd2c58b3740bf5d4f
Israel Ochoa <israelgochoa@gmail.com>
2022-09-23 23:50:02 -0500

    commit 2
    Files upload, no images

---
+++ ResponsiveAdminDashboard_ThemesLightDark/orders.js
@@ -0,0 +1,37 @@
+const Orders=[
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        productStatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'LARVENDER KF192 Drone',
+        productNumber:'36378',
+        productStatus:'Refunded',
+        shipping:'Declined'
+
+    },
+    {
+        productName:'Ruko F11 Pro Drone',
+        productNumber:'49347',
+        productStatus:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Drone with Camera Drone',
+        productNumber:'96996',
+        productStatus:'Paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName:'GPS 4k Drone',
+        productNumber:'22821',
+        productStatus:'Paid',
+        shipping:'Delivered'
+
+    },
+]
