4b20042f641c122c621eda5c7be9979f384972c6
Israel Ochoa <israelgochoa@gmail.com>
2022-10-12 21:15:06 -0400

    Add files via upload

---
+++ React/Responsive_Restaurant_Garitch/src/container/Intro/Intro.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react';
+import{BsFillPlayFill,BsPauseFill} from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
+
+const Intro = () => {
+  const [playVideo,setPlayVideo]=useState(false);
+  const vidRef=useRef();
+  const handleVideo=()=>{
+    setPlayVideo(
+      (prevPlayVideo)=>!prevPlayVideo
+      );
+      if(playVideo){
+        vidRef.current.pause();
+      }else{
+        vidRef.current.play();
+      }
+  }
+  return(
+  <div className='app__video'>
+    <video src={meal}
+           type="video/mp4"
+           ref={vidRef}
+           loop
+           controls={false}
+           muted
+    />
+    <div className="app__video-overlay 
+    flex__center">
+      <div className="app__video-overlay_circle 
+    flex__center"
+          onClick={handleVideo}>
+            {
+              playVideo
+              ? <BsPauseFill color="var(--color-white)"
+                            fontSize={30}/>
+              : <BsFillPlayFill color="var(--color-golden)"
+                              fontSize={30}/>                             
+            }
+      </div>
+    </div>        
+           
+  </div>
+);}
+
+export default Intro;
